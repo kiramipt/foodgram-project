dc127039e58cb6320eee6e6afbd58a4d89e0dcd9
k.puchkov <U098iolk>
2020-10-25 19:27:30 +0300

    fix review tips

--- recipes/views.py
+++ recipes/views.py
@@ -253,3 +253,20 @@ def purchases_download(request):
     response["Content-Disposition"] = "attachment; filename='purchases.txt'"
 
     return response
+
+
+def page_not_found(request, exception):
+    return render(
+        request,
+        "misc/404.html",
+        {"path": request.path},
+        status=404
+    )
+
+
+def server_error(request):
+    return render(
+        request,
+        "misc/500.html",
+        status=500
+    )
